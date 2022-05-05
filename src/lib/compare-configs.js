ccf7d93a50fee6d462ddb9424fccec9536690c0a
Fortunat Mutunda <fotimutunda@gmail.com>
2022-05-05 03:45:53 +0300

    Replace all tabs with sapces.

--- src/lib/compare-configs.js
+++ src/lib/compare-configs.js
@@ -3,9 +3,9 @@ const getLocalConfigs = require('../lib/configs');
 const path = require('path');
 
 async function compare(herokuApiToken, herokuAppName, localConfigsPath) {
-	const herokuConfigs = await getHerokuConfigs(herokuApiToken, herokuAppName);
-	const localConfigs = await getLocalConfigs(path.resolve(localConfigsPath));
-	return localConfigs.filter(localConfig => !herokuConfigs.includes(localConfig));
+  const herokuConfigs = await getHerokuConfigs(herokuApiToken, herokuAppName);
+  const localConfigs = await getLocalConfigs(path.resolve(localConfigsPath));
+  return localConfigs.filter(localConfig => !herokuConfigs.includes(localConfig));
 }
 
 module.exports = compare;
